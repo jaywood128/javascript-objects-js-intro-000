3d05ca8e68f91a1f7e599b091cb3915eb18e8ab9
Johnathon Wood <jaywood128@gmail.com>
2018-09-07 22:07:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,7 @@ var playlist = {
   'artist name': 'song title',
 }
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {[artistname]: 'songtitle'})
+  playlist[artist] = song
+
+  return playlist
 }
